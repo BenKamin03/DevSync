99240f80047568f18dca7c8ddcfbd067c29ea2ba
BenKamin03 <88076233+BenKamin03@users.noreply.github.com>
2025-02-28 19:44:50 -0800

    opening on correct side and test state management update

--- src/extension.ts
+++ src/extension.ts
@@ -1,95 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { join } from "path";
 import * as vscode from "vscode";
-import { ExtensionContext, ExtensionMode, Uri, Webview } from "vscode";
+import { ExtensionContext, ExtensionMode, Uri, Webview, WebviewOptions } from "vscode";
 import { MessageHandlerData } from "@estruyf/vscode";
 import { readFileSync } from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "vscode-react-webview-starter.openWebview",
-    () => {
-      const panel = vscode.window.createWebviewPanel(
-        "react-webview",
-        "React Webview",
-        vscode.ViewColumn.One,
-        {
-          enableScripts: true,
-          retainContextWhenHidden: true,
-        }
-      );
-
-      panel.webview.onDidReceiveMessage(
-        (message) => {
-          const { command, requestId, payload } = message;
-
-          if (command === "GET_DATA") {
-            // Do something with the payload
-
-            // Send a response back to the webview
-            panel.webview.postMessage({
-              command,
-              requestId, // The requestId is used to identify the response
-              payload: `Hello from the extension!`,
-            } as MessageHandlerData<string>);
-          } else if (command === "GET_DATA_ERROR") {
-            panel.webview.postMessage({
-              command,
-              requestId, // The requestId is used to identify the response
-              error: `Oops, something went wrong!`,
-            } as MessageHandlerData<string>);
-          } else if (command === "POST_DATA") {
-            vscode.window.showInformationMessage(
-              `Received data from the webview: ${payload.msg}`
-            );
-          }
-        },
-        undefined,
-        context.subscriptions
-      );
-
-      panel.webview.html = getWebviewContent(context, panel.webview);
-    }
-  );
-
-  context.subscriptions.push(disposable);
+    const provider = new ReactWebviewViewProvider(context);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider("vscode-react-webview-starter.view", provider));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
 
 const getWebviewContent = (context: ExtensionContext, webview: Webview) => {
-  const jsFile = "main.bundle.js";
-  const localServerUrl = "http://localhost:9000";
+    const jsFile = "main.bundle.js";
+    const localServerUrl = "http://localhost:9000";
 
-  let scriptUrl = [];
-  let cssUrl = null;
+    let scriptUrl = [];
+    let cssUrl = null;
 
-  const isProduction = context.extensionMode === ExtensionMode.Production;
-  if (isProduction) {
-    // Get the manifest file from the dist folder
-    const manifest = readFileSync(
-      join(context.extensionPath, "dist", "webview", "manifest.json"),
-      "utf-8"
-    );
-    const manifestJson = JSON.parse(manifest);
-    for (const [key, value] of Object.entries<string>(manifestJson)) {
-      if (key.endsWith(".js")) {
-        scriptUrl.push(
-          webview
-            .asWebviewUri(
-              Uri.file(join(context.extensionPath, "dist", "webview", value))
-            )
-            .toString()
-        );
-      }
+    const isProduction = context.extensionMode === ExtensionMode.Production;
+    if (isProduction) {
+        const manifest = readFileSync(join(context.extensionPath, "dist", "webview", "manifest.json"), "utf-8");
+        const manifestJson = JSON.parse(manifest);
+        for (const [key, value] of Object.entries<string>(manifestJson)) {
+            if (key.endsWith(".js")) {
+                scriptUrl.push(webview.asWebviewUri(Uri.file(join(context.extensionPath, "dist", "webview", value))).toString());
+            }
+        }
+    } else {
+        scriptUrl.push(`${localServerUrl}/${jsFile}`);
     }
-  } else {
-    scriptUrl.push(`${localServerUrl}/${jsFile}`);
-  }
 
-  return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
@@ -103,3 +45,48 @@ const getWebviewContent = (context: ExtensionContext, webview: Webview) => {
 	</body>
 	</html>`;
 };
+
+class ReactWebviewViewProvider implements vscode.WebviewViewProvider {
+    private context: vscode.ExtensionContext;
+
+    constructor(context: vscode.ExtensionContext) {
+        this.context = context;
+    }
+
+    resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken) {
+        webviewView.webview.options = {
+            enableScripts: true,
+            retainContextWhenHidden: true,
+        } as WebviewOptions;
+
+        webviewView.webview.html = getWebviewContent(this.context, webviewView.webview);
+
+        setTimeout(() => {
+            webviewView.webview.postMessage({ type: "UPDATE_STATE", payload: { count: 5 } });
+        }, 2000);
+
+        webviewView.webview.onDidReceiveMessage(
+            (message) => {
+                const { command, requestId, payload } = message;
+
+                if (command === "GET_DATA") {
+                    webviewView.webview.postMessage({
+                        command,
+                        requestId,
+                        payload: `Hello from the extension!`,
+                    } as MessageHandlerData<string>);
+                } else if (command === "GET_DATA_ERROR") {
+                    webviewView.webview.postMessage({
+                        command,
+                        requestId,
+                        error: `Oops, something went wrong!`,
+                    } as MessageHandlerData<string>);
+                } else if (command === "POST_DATA") {
+                    vscode.window.showInformationMessage(`Received data from the webview: ${payload.msg}`);
+                }
+            },
+            undefined,
+            this.context.subscriptions
+        );
+    }
+}
--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -1,54 +1,76 @@
-import * as React from 'react';
-import { messageHandler } from '@estruyf/vscode/dist/client';
+import * as React from "react";
+import { messageHandler } from "@estruyf/vscode/dist/client";
 import "./styles.css";
+import { useEffect } from "react";
 
 export interface IAppProps {}
 
-export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChildren<IAppProps>) => {
-  const [message, setMessage] = React.useState<string>("");
-  const [error, setError] = React.useState<string>("");
+export const App: React.FunctionComponent<IAppProps> = ({}: React.PropsWithChildren<IAppProps>) => {
+    const [message, setMessage] = React.useState<string>("");
+    const [error, setError] = React.useState<string>("");
+    const [count, setCount] = React.useState<number>(0);
 
-  const sendMessage = () => {
-    messageHandler.send('POST_DATA', { msg: 'Hello from the webview' });
-  };
+    useEffect(() => {
+        const handleMessage = (event: MessageEvent) => {
+            const message = event.data;
+            if (message.type === "UPDATE_STATE") {
+                setCount(message.payload.count);
+            }
+        };
 
-  const requestData = () => {
-    messageHandler.request<string>('GET_DATA').then((msg) => {
-      setMessage(msg);
-    });
-  };
+        window.addEventListener("message", handleMessage);
 
-  const requestWithErrorData = () => {
-    messageHandler.request<string>('GET_DATA_ERROR')
-    .then((msg) => {
-      setMessage(msg);
-    })
-    .catch((err) => {
-      setError(err);
-    });
-  };
+        return () => {
+            window.removeEventListener("message", handleMessage);
+        };
+    }, []);
 
-  return (
-    <div className='app'>
-      <h1>Hello from the React Webview Starter</h1>
+    const sendMessage = () => {
+        messageHandler.send("POST_DATA", { msg: "Hello from the webview" });
+    };
 
-      <div className='app__actions'>
-        <button onClick={sendMessage}>
-          Send message to extension
-        </button>
+    const requestData = () => {
+        messageHandler.request<string>("GET_DATA").then((msg) => {
+            setMessage(msg);
+        });
+    };
 
-        <button onClick={requestData}>
-          Get data from extension
-        </button>
+    const requestWithErrorData = () => {
+        messageHandler
+            .request<string>("GET_DATA_ERROR")
+            .then((msg) => {
+                setMessage(msg);
+            })
+            .catch((err) => {
+                setError(err);
+            });
+    };
 
-        <button onClick={requestWithErrorData}>
-          Get data with error
-        </button>
-      </div>
+    return (
+        <div className="app">
+            <h1>Hello from the React Webview Starter</h1>
 
-      {message && <p><strong>Message from the extension</strong>: {message}</p>}
+            <div className="app__actions">
+                <button onClick={sendMessage}>Send message to extension</button>
 
-      {error && <p className='app__error'><strong>ERROR</strong>: {error}</p>}
-    </div>
-  );
+                <button onClick={requestData}>Get data from extension</button>
+
+                <button onClick={requestWithErrorData}>Get data with error</button>
+            </div>
+
+            {message && (
+                <p>
+                    <strong>Message from the extension</strong>: {message}
+                </p>
+            )}
+
+            {error && (
+                <p className="app__error">
+                    <strong>ERROR</strong>: {error}
+                </p>
+            )}
+
+            <h1>Count: {count}</h1>
+        </div>
+    );
 };
